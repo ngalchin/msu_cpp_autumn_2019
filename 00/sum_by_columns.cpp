b588971b3e65ce3c096d97017388a2912817745c
ngalchin <32197952+ngalchin@users.noreply.github.com>
2019-10-06 18:51:26 +0300

    Create sum_by_columns.cpp

---
+++ 00/sum_by_columns.cpp
@@ -0,0 +1,82 @@
+#include <chrono>
+#include <iostream>
+
+#define N 10000
+
+class Timer
+{
+    using clock_t = std::chrono::high_resolution_clock;
+    using microseconds = std::chrono::microseconds;
+public:
+    Timer()
+        : start_(clock_t::now())
+    {
+    }
+
+    ~Timer()
+    {
+        const auto finish = clock_t::now();
+        const auto us = 
+            std::chrono::duration_cast<microseconds>
+                (finish - start_).count();
+        std::cout << us << " us" << std::endl;
+    }
+
+private:
+    const clock_t::time_point start_;
+};
+
+void init_2(int** A)
+{
+        for (int i = 0; i < N; i++)
+        {
+                for (int j = 0; j < N; j++)
+                {
+                        A[i][j] = 3*i +2*j + 1;
+                }
+        }
+}
+
+int sum_by_columns(int** A)
+{
+        Timer t;
+
+        int sum;
+
+        for (int j = 0; j < N; j++)
+        {
+                for (int i = 0; i < N; i++)
+                {
+                        sum += A[i][j];
+                }
+
+        }
+
+        return sum;
+
+}
+
+
+
+int main()
+{
+
+        int** matrix = new int*[N];
+        for(size_t i = 0 ; i < N ; ++i)
+                matrix[i] = new int[N];
+
+        int result;
+
+        init_2(matrix);
+
+        result = sum_by_columns(matrix);
+
+
+        for(size_t i = 0 ; i < N ; ++i)
+                delete matrix[i];
+        delete[] matrix;
+
+        return 0;
+
+}
+
